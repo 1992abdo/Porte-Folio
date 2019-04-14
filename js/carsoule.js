0d3a9fe951200653e87d0d854ade2d76d36abe8f
@ABDELATI <chraiha1abdelati@gmail.com>
2019-04-14 15:18:05 +0100

    Porte-Folio

---
+++ js/carsoule.js
@@ -0,0 +1,53 @@
+// nav bar color active
+// $("a").click(function(){
+//   $("a").css("background-color","")
+//   $("this").css("background-color","black")
+// });
+
+$('.time').carousel({
+    interval: 20000
+  })
+
+// script when i scroll the page  scroll it will be slawly
+$(document).ready(function () {
+  // Add scrollspy to <body>
+  $('body').scrollspy({ target: ".navbar", offset: 50 });
+
+  // Add smooth scrolling on all links inside the navbar
+  $("#navbarSupportedContent a").on('click', function (event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function () {
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    }  // End if
+  });
+});
+
+
+// partie popup de button
+(function($){
+  $(document).ready(function() {
+      $('.popup-youtube').magnificPopup({
+          disableOn: 100,
+          type: 'iframe',
+          mainClass: 'mfp-fade',
+          removalDelay: 160,
+          preloader: false,
+          fixedContentPos: false
+      });
+  });
+  })(jQuery)
+
